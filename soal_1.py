f49547ae8168d289314d0f9e029f7217f94524c9
Izalhaasclaw <isman.faizal2006@gmail.com>
2024-11-13 12:06:35 +0700

    Add files via upload

---
+++ soal_1.py
@@ -0,0 +1,11 @@
+a = 10
+b = 5
+
+hasil = a+b
+print(hasil)
+
+hasil = a-b
+print(hasil)
+
+hasil = a*b
+print(hasil)
---
+++ soal_3.py
@@ -0,0 +1,6 @@
+jumlah_barang = ("jumlah_barang :")
+harga_barang_ke1 = 1000000
+harga_barang_ke2 = 2000000
+
+hasil = harga_barang_ke1 + harga_barang_ke2
+print(hasil)
